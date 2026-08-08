619d9d94364d4d078c34f37fcc2e8af73d193569
Ajay Kumar <2023ugcs091@nitjsr.ac.in>
2026-08-08 19:28:23 +0530

    Time: 0 ms (100%), Space: 22.6 MB (53.72%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        set<int>st;
-        //Insert all element into set ->set takes unique element into it 
-        for(int i =0;i<nums.size();i++){
-            st.insert(nums[i]);
+
+        //Brute force  ->set
+        // set<int>st;
+        // //Insert all element into set ->set takes unique element into it 
+        // for(int i =0;i<nums.size();i++){
+        //     st.insert(nums[i]);
+        // }
+        // //Traverse whole set and put all set elements into that array 
+        // int index=0;
+        // for(auto it :st){
+        //     nums[index]=it;
+        //     index++;
+        // }
+        // return index;  // because index last me pahuch gya hoga 
+
+    //optimal->2 pointer
+     int i=0;
+     
+     for(int j=1;j<nums.size();j++){
+        if(nums[j]!=nums[i]){
+            i++;
+            nums[i]=nums[j];
+            }
         }
-        //Traverse whole set and put all set elements into that array 
-        int index=0;
-        for(auto it :st){
-            nums[index]=it;
-            index++;
-        }
-        return index;
+     return i+1;
     }
 };
